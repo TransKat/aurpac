8ade548b922a5fb8a9a8c4cd390a62cae01a6702
Kat <61893437+TransKat@users.noreply.github.com>
2021-11-16 21:39:08 -0600

    1.0 commit

---
+++ aurpac.py
@@ -0,0 +1,43 @@
+## AURPAC: Simple AUR helper in Python
+## vesrion 1.0
+
+import os
+import sys
+#import modules
+
+#change to home directory to ensure packages arent cloned in the /usr/bin folder
+user = os.getlogin()
+os.chdir(f"/home/{user}")
+
+
+#get the pkg and send it to clone.py
+try:
+    targetpkg = str(sys.argv[1]) #store command line arg
+    print(f"Package to install: {targetpkg}")
+
+except:
+    print("You did not provide a package to install.")
+    sys.exit()
+
+#clone the repo from the AUR using git
+try:
+    os.system(f"git clone https://aur.archlinux.org/{targetpkg}.git")
+except:
+    print("Something went wrong. You may not have git installed. The AUR may be down or the package may not exist. Try again.")
+    sys.exit()
+
+# move to the pkg directory
+try:
+    wdir = os.getcwd()
+    print(wdir)
+    os.chdir(f"{wdir}/{targetpkg}")
+except:
+    print(f"fatal error: no directory was made for {targetpkg}.")
+    sys.exit()
+
+# install it
+try:
+    os.system("makepkg -si")
+except:
+    print("Something went wrong. try again.")
+    sys.exit
